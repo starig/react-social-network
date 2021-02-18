c5222bbfc2ab22126df06b2ab2c1b3c2e4c977cb
starigg <starkovivan3@gmail.com>
2021-02-19 00:05:09 +0300

    Installed axios, added server query on the users page

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -4,39 +4,7 @@ const SET_USERS = 'SET-USERS';
 
 
 let initialState = {
-    usersData: [{
-        id: 1,
-        name: 'Ivan Starkov',
-        status: 'Hello world!',
-        location: {city: 'Saint-Petersburg', country: 'Russia'},
-        avatar: 'https://sun9-29.userapi.com/impg/1t7Rn5JZBywfXiXV5ErJwxZMrq8SJvPNsEfBTw/BcfMZx1jV9o.jpg?size=1080x1080&quality=96&proxy=1&sign=af86d3b8be3d796411fbc39899ead623&type=album',
-        followed: true,
-    },
-        {
-            id: 2,
-            name: 'Andrey Ten',
-            status: 'King Kong!',
-            location: {city: 'Saint-Petersburg', country: 'Russia'},
-            avatar: 'https://sun9-58.userapi.com/impg/dyU8px7NgSx9JwKXf7b6lHFvBW8NA45KhakD4Q/vqJTY4I08A0.jpg?size=637x839&quality=96&proxy=1&sign=98a3b5b5ab2af2435555637c13d411b2&type=album',
-            followed: true,
-        },
-        {
-            id: 3,
-            name: 'Sofia Protasova',
-            status: 'Doctor!',
-            location: {city: 'Khabarovsk', country: 'Russia'},
-            avatar: 'https://sun9-37.userapi.com/impg/To82glkn4N-kvUn-1I4Rbmq8NaZnXqHFOX1Wlg/glE5PZ9ishI.jpg?size=900x900&quality=96&proxy=1&sign=4e1eb3cf42a0377feec07cfcf3a79a05&type=album',
-            followed: false,
-        },
-        {
-            id: 4,
-            name: 'Alexander Maraktaev',
-            status: 'Gamer!',
-            location: {city: 'Ulan-Ude', country: 'Russia'},
-            avatar: 'https://sun9-15.userapi.com/impg/xLRVxu5UIAbjCzYvSddpux12mt0RrxcSt22iSw/ueKE_olFwyI.jpg?size=607x607&quality=96&proxy=1&sign=fac2b7ce254b635fba2a54fdb1f7036c&type=album',
-            followed: false,
-        },
-    ],
+    usersData: [ ]
 }
 
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from "axios";
+import userPhoto from './../../assets/img/avatarDefault.png';
 
 
 let Users = (props) => {
+
+    if (props.usersData.length === 0) {
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
+
+    }
+
     return <div className={styles.users}>
         {
-            props.usersData.map( user => <div key={user.id} className={styles.item}>
-                <div className={styles.avatar}>
-                    <img alt={`avatar`} src={user.avatar} className={styles.avatarImg}/>
-                    {
-                        user.followed
-                            ? <div className={styles.unfollowButton} onClick={() => {props.unfollow(user.id)}} >unfollow</div>
-                            : <div className={styles.followButton} onClick={() => {props.follow(user.id)}}>follow</div>
-                    }
+            props.usersData.map(user => <div key={user.id} className={styles.item}>
+                    <div className={styles.avatar}>
+                        <img alt={`avatar`} src={ user.photos.small != null
+                            ? user.photos.small : userPhoto} className={styles.avatarImg}/>
+                        {
+                            user.followed
+                                ? <div className={styles.unfollowButton} onClick={() => {
+                                    props.unfollow(user.id)
+                                }}>unfollow</div>
+                                : <div className={styles.followButton} onClick={() => {
+                                    props.follow(user.id)
+                                }}>follow</div>
+                        }
+                    </div>
+                    <div className={styles.box}>
+                        <div className={styles.name}>{user.name}</div>
+                        <div className={styles.location}>{`user.location.city + ', ' + user.location.country`}</  div>
+                        <div className={styles.status}>{user.status}</div>
+                    </div>
                 </div>
-                <div className={styles.box}>
-                    <div className={styles.name}>{user.name}</div>
-                    <div className={styles.location}>{user.location.city + ', ' + user.location.country}</  div>
-                    <div className={styles.status}>{user.status}</div>
-                </div>
-            </div>
             )
         }
     </div>
